45be33ddc81ebd25a0582cc72631247f3a5846c5
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-08-29 23:24:26 -0700

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
             }
 
             val flutterSdkPath = properties.getProperty("flutter.sdk")
-
             require(flutterSdkPath != null) {
                 "flutter.sdk not set in local.properties"
             }
@@ -26,22 +25,21 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+
+    id("com.android.application") version "8.13.2" apply false
+
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(
-        RepositoriesMode.FAIL_ON_PROJECT_REPOS
-    )
-
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "power_fan_network"
+rootProject.name = "power-fan-network"
 
 include(":app")
