5050ba007b79df09f90e0e47a441d1087de9d4ec
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-08-29 23:08:27 -0700

    Create build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.fanmining.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.fanmining.app"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
