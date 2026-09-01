00867f0fd8dd25995f9bd5ff90cdf21cb025d145
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-01 06:30:22 -0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
+        multiDexEnabled = true
     }
 
     compileOptions {
@@ -34,4 +35,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
 }
