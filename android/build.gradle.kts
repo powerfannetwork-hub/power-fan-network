f05d8ebb766acaf0aca70e9e96d9dab8620d518c
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-02 15:00:51 +0100

    Set compile SDK version to 36 for library modules
    Force Android library modules to compile against API 36.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,12 @@
+import com.android.build.api.dsl.LibraryExtension
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.buildDir = file("../build")
 
 subprojects {
@@ -6,6 +15,23 @@ subprojects {
 
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+/*
+ * POWER FAN NETWORK
+ *
+ * AppLovin MAX currently has an Android library module
+ * that may declare an older compile SDK.
+ *
+ * Force Android library modules, including applovin_max,
+ * to compile against API 36.
+ */
+subprojects {
+    plugins.withId("com.android.library") {
+        extensions.configure<LibraryExtension> {
+            compileSdk = 36
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
