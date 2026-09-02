7a1b17dbb1289be293ea10d421ce1f36a16a55bc
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-02 15:19:03 +0100

    Refactor settings.gradle.kts for better readability

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,7 +7,8 @@ pluginManagement {
                 properties.load(it)
             }
 
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            val flutterSdkPath =
+                properties.getProperty("flutter.sdk")
 
             require(flutterSdkPath != null) {
                 "flutter.sdk not set in local.properties"
@@ -16,7 +17,9 @@ pluginManagement {
             flutterSdkPath
         }
 
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    includeBuild(
+        "$flutterSdkPath/packages/flutter_tools/gradle"
+    )
 
     repositories {
         google()
@@ -26,24 +29,26 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("dev.flutter.flutter-plugin-loader")
+        version "1.0.0"
 
-    id("com.android.application") version "8.13.2" apply false
+    id("com.android.application")
+        version "8.13.2"
+        apply false
 
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.android")
+        version "2.2.21"
+        apply false
 
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services")
+        version "4.4.4"
+        apply false
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-
     repositories {
         google()
         mavenCentral()
-
-        // Flutter engine and Android artifacts
-        maven("https://storage.googleapis.com/download.flutter.io")
     }
 }
 
