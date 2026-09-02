b0367c25945f4baf22d28456bd068ddf04df8fb6
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-02 15:41:34 +0100

    Add core library desugaring support
    Enable core library desugaring for compatibility.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,6 +20,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+
+        // Required by flutter_local_notifications
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -49,4 +52,7 @@ flutter {
 
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
+
+    // Required for core library desugaring
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
