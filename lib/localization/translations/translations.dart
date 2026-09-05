f18b0b8bffa020ce42e8e26b8e46b6594ed05620
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-05 15:47:34 +0100

    Create translations.dart for language support
    Added localization support for multiple languages.

---
+++ lib/localization/translations/translations.dart
@@ -0,0 +1,23 @@
+import 'en.dart';
+import 'zh.dart';
+import 'es.dart';
+import 'fr.dart';
+import 'ar.dart';
+import 'hi.dart';
+import 'bn.dart';
+import 'ru.dart';
+import 'tr.dart';
+import 'id.dart';
+
+const Map<String, Map<String, String>> translations = {
+  'en': enTranslations,
+  'zh': zhTranslations,
+  'es': esTranslations,
+  'fr': frTranslations,
+  'ar': arTranslations,
+  'hi': hiTranslations,
+  'bn': bnTranslations,
+  'ru': ruTranslations,
+  'tr': trTranslations,
+  'id': idTranslations,
+};
