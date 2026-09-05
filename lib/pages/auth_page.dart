a59d105c047ec368e0fb0bfda81409836c374a8d
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-05 01:05:15 +0100

    Simplify AuthPage to only show LoginPage
    Removed the page switcher functionality and simplified the AuthPage to always show the LoginPage.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -3,9 +3,7 @@
 import 'package:flutter/material.dart';
 
 import '../localization/app_localizations.dart';
-import '../localization/language_controller.dart';
 import 'login_page.dart';
-import 'register_page.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -15,53 +13,8 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  bool _showLogin = true;
-
-  void _switchPage() {
-    setState(() {
-      _showLogin = !_showLogin;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final t = AppLocalizations.of(context);
-
-    return Scaffold(
-      backgroundColor: const Color(0xFFF8F8FC),
-      body: Stack(
-        children: [
-          AnimatedSwitcher(
-            duration: const Duration(milliseconds: 250),
-            child: _showLogin
-                ? const LoginPage(key: ValueKey('login'))
-                : const RegisterPage(key: ValueKey('register')),
-          ),
-
-          Positioned(
-            left: 24,
-            right: 24,
-            bottom: 18,
-            child: SafeArea(
-              top: false,
-              child: Center(
-                child: TextButton(
-                  onPressed: _switchPage,
-                  child: Text(
-                    _showLogin
-                        ? t.translate('createAccount')
-                        : t.translate('alreadyHaveAccount'),
-                    style: const TextStyle(
-                      color: Color(0xFF3B159B),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return const LoginPage();
   }
 }
