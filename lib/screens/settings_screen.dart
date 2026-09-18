e53726a398069b3d6be90c426e52838e21d8ee62
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-18 06:25:57 +0100

    Remove shared_preferences import from settings_screen
    Removed unused import for shared_preferences package.

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../services/auth_service.dart';
